135e694818b6be9edc6b58955803102c290596d6
Alana Cristyna Feitosa Dias <alanafeitosa84996@gmail.com>
2026-06-11 14:32:55 -0300

    feat: adiciona classe Quadro

---
+++ src/models/quadro.py
@@ -0,0 +1,22 @@
+from src.models.tarefa import tarefaAcademica, tarefaPessoal
+class Quadro:
+    def __init__(self, titulo, filtro):
+        self.__tarefas = []
+        self.__titulo = titulo
+        self.__filtro = filtro
+    def get_tarefas(self):
+        return self.__tarefas
+    def set_tarefas(self, tarefa):
+        self.__tarefas = tarefa
+    def adicionar_tarefa(self, tarefa):
+        self.get_tarefas().append(tarefa)
+    def remover_tarefa(self, tarefa):
+        self.get_tarefas().remove(tarefa)
+    def listar_tarefas(self):
+        return self.get_tarefas()
+    def filtrarPorTipo (self, tipo):
+        tarefas_filtradas = []
+        for p in self.__tarefas:
+            if isinstance (p, tipo):
+                tarefas_filtradas.append(p)
+            return tarefas_filtradas
