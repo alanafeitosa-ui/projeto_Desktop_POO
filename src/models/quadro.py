fad3db947063c69d34e27bece8ef52954917c9e3
Alana Cristyna Feitosa Dias <alanafeitosa84996@gmail.com>
2026-07-04 13:52:21 -0300

    refactor: complementa type hints na classe Quadro

--- src/models/quadro.py
+++ src/models/quadro.py
@@ -6,11 +6,11 @@ class Quadro:
         self.__titulo = titulo
     def get_tarefas(self) -> List[Tarefa]:
         return self.__tarefas
-    def set_tarefas(self, tarefa: List[Tarefa]) -> None:
-        self.__tarefas = tarefa
-    def adicionar_tarefa(self, tarefa: Tarefa):
+    def set_tarefas(self, tarefas: List[Tarefa]) -> None:
+        self.__tarefas = tarefas
+    def adicionar_tarefa(self, tarefa: Tarefa) -> None:
         self.get_tarefas().append(tarefa)
-    def remover_tarefa(self, tarefa: Tarefa):
+    def remover_tarefa(self, tarefa: Tarefa) -> None:
         self.get_tarefas().remove(tarefa)
     def listar_tarefas(self) -> List[Tarefa]:
         return self.get_tarefas()
