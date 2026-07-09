52f24ca932f496fcdf7ac2c7cc6e5e938ec485ef
Alana Cristyna Feitosa Dias <alanafeitosa84996@gmail.com>
2026-07-09 16:11:30 -0300

    feat: implementa metodo verificar prazo no menu

--- src/views/menu.py
+++ src/views/menu.py
@@ -6,11 +6,16 @@ class Menu:
     def __init__(self):
         self.__quadro = None
         self.executar()
+    def __verificar_quadro (self) -> bool :
+        if self.__quadro is None:
+            print("\nNecessário criar um quadro antes de realizar esta ação!\n")
+            return False
+        return True
     def executar(self):
         while True:
             print("\n===== MENU GERENCIAMENTO =====")
             print("1 - Criar quadro")
-            print("2 - Criar tarefa academica")
+            print("2 - Criar tarefa acadêmica")
             print("3 - Criar tarefa Pessoal")
             print("4 - Filtrar tarefas")
             print("5 - Remover tarefa")
@@ -18,7 +23,8 @@ class Menu:
             print("7 - Alterar status para concluído")
             print("8 - Listar tarefas")
             print("9 - Exibir detalhes das tarefas")
-            print("10 - Parar programa")
+            print("10 - verificar prazo de alguma tarefa acadêmica")
+            print("11 - Parar programa")
             opcao = str(input("Insira a seguir uma das opções acima: ").strip())
 
             match opcao:
@@ -27,8 +33,8 @@ class Menu:
                     self.__quadro = Quadro(nome_quadro)
 
                 case "2":
-                    if self.__quadro is None:
-                        print("Necessário criar um quadro antes de realizar esta ação!\n")
+                    if not self.__verificar_quadro():
+                        continue
                     else:
                         quantia_tarefa_academica = int(input("Insira a seguir a quantidade de tarefas acadêmicas que deseja criar: ").strip())
                         tarefas_academicas = []
@@ -40,7 +46,7 @@ class Menu:
                             disciplina = str(input("Insira a seguir a disciplina referente a tarefa: ").strip())
                             data_entrega = str(input("Insira a data de entrega da tarefa no formato (dd/mm/aaaa): ").strip())
                             data_convertida = datetime.strptime(data_entrega, "%d/%m/%Y").date()
-                            prioridade = str(input("Insira a seguir a ordem de prioridade da tarefa: ").strip())
+                            prioridade = str(input("Insira a seguir o nível de prioridade da tarefa: ").strip())
                             
                             tarefa_acad = TarefaAcademica(titulo, descricao, status, disciplina, data_convertida, prioridade)
                             tarefas_academicas.append(tarefa_acad)
@@ -48,8 +54,8 @@ class Menu:
                             self.__quadro.adicionar_tarefa(tarefa)
 
                 case "3":
-                    if self.__quadro is None:
-                        print("Necessário criar um quadro antes de realizar esta ação!\n")
+                    if not self.__verificar_quadro():
+                        continue
                     else:
                         quantia_tarefa_pessoal = int(input("\nInsira a seguir a quantidade de tarefas pessoais que deseja criar: ").strip())
                         tarefas_pessoais = []
@@ -57,18 +63,18 @@ class Menu:
                             print(f"\n=== Cadastro tarefa pessoal {i + 1} ===")
                             titulo = str(input("Insira a seguir o titulo da tarefa: ").strip())
                             descricao = str(input("Insira a seguir uma breve descrição sobre a tarefa: ").strip())
-                            status = str(input("A tarefa está: a concluir, em andamento ou concluída?\n").strip())
+                            status = str(input("A tarefa está: a concluir, em andamento ou concluída?").strip())
                             tarefa_pes = TarefaPessoal(titulo, descricao, status)
                             tarefas_pessoais.append(tarefa_pes)
                         for tarefa in tarefas_pessoais:
                             self.__quadro.adicionar_tarefa(tarefa)
 
                 case "4":
-                    if self.__quadro is None:
-                        print("Necessário criar um quadro antes de realizar esta ação!\n")
+                    if not self.__verificar_quadro():
+                        continue
                     else:
-                        tipo = input("Deseja filtrar as tarefas pelo tipo tarefa academica ou tarefa pessoal? ").lower().strip()
-                        if tipo == "tarefa academica":
+                        tipo = input("Deseja filtrar as tarefas pelo tipo tarefa acadêmica ou tarefa pessoal? ").lower().strip()
+                        if tipo == "tarefa academica" or tipo == "tarefa acadêmica":
                             tarefas = self.__quadro.filtrar_por_tipo(TarefaAcademica)
                         elif tipo == "tarefa pessoal":
                             tarefas = self.__quadro.filtrar_por_tipo(TarefaPessoal)
@@ -79,69 +85,89 @@ class Menu:
                             print(f"{tarefa}\n")
 
                 case "5":
-                    if self.__quadro is None:
-                        print("\nNecessário criar um quadro antes de realizar esta ação!")
+                    if not self.__verificar_quadro():
+                        continue
                     else:
-                        if len(self.__quadro.listar_tarefas()) == 0:
+                        tarefas = self.__quadro.listar_tarefas()
+                        if len(tarefas) == 0:
                             print("Não existem tarefas para remover, cadastre alguma antes de realizar a ação!")
                         else:
                             print("\n=== Tarefas a remover ===")
-                            for i, tarefa in enumerate(self.__quadro.listar_tarefas(), start=1):
+                            for i, tarefa in enumerate(tarefas, start=1):
                                 print(f"{i}. {tarefa}\n")
                             indice = int(input("\nInsira a seguir o indice da tarefa que deseja remover: ").strip())
-                            if 1 < indice <= len(self.__quadro.listar_tarefas()):
-                                self.__quadro.remover_tarefa(self.__quadro.listar_tarefas()[indice - 1])
+                            if 1 <= indice <= len(tarefas):
+                                self.__quadro.remover_tarefa(tarefas[indice - 1])
+                                print("Tarefa removida com sucesso!")
                             else:
                                 print("Indice inválido")
 
                 case "6": 
-                    if self.__quadro is None:
-                        print("\nNecessário criar um quadro antes de realizar esta ação!")
+                    if not self.__verificar_quadro():
+                        continue
                     else:
-                        if len(self.__quadro.listar_tarefas()) == 0:
+                        tarefas = self.__quadro.listar_tarefas()
+                        if len(tarefas) == 0:
                             print("\nNenhuma tarefa cadastrada")
                         else:
                             print("\n=== Tarefas === ")
-                            for i, tarefa in enumerate(self.__quadro.listar_tarefas(), start=1):
+                            for i, tarefa in enumerate(tarefas, start=1):
                                 print (f"{i}. {tarefa.exibir_detalhes()}\n")
                             indice = int(input("\nInsira a seguir o indice da tarefa que deseja alterar o status para \"em andamento\": ").strip())
-                            if 1<= indice <= len(self.__quadro.listar_tarefas()):
-                                self.__quadro.listar_tarefas()[indice - 1].mover_para_andamento()
+                            if 1<= indice <= len(tarefas):
+                                tarefas[indice - 1].mover_para_andamento()
+                                print("Status alterado para \"em andamento\"!")
                             else:
                                 print("Indice inválido")
 
                 case "7":
-                    if self.__quadro is None:
-                        print("Necessário criar um quadro antes de realizar esta ação!")
+                    if not self.__verificar_quadro():
+                        continue
                     else:
-                        if len(self.__quadro.listar_tarefas()) == 0:
+                        tarefas = self.__quadro.listar_tarefas()
+                        if len(tarefas) == 0:
                             print("\nNenhuma tarefa cadastrada")
                         else:
                             print("\n=== Tarefas === ")
-                            for i, tarefa in enumerate(self.__quadro.listar_tarefas(), start=1):
+                            for i, tarefa in enumerate(tarefas, start=1):
                                 print (f"{i}. {tarefa.exibir_detalhes()}\n")
                             indice = int(input("\nInsira a seguir o indice da tarefa que deseja alterar o status para \"concluído\": ").strip())
-                            if 1<= indice <= len(self.__quadro.listar_tarefas()):
-                                self.__quadro.listar_tarefas()[indice - 1].mover_para_concluido()
+                            if 1<= indice <= len(tarefas):
+                                tarefas[indice - 1].mover_para_concluido()
+                                print("Status alterado para \"concluído\"!")
                             else:
                                 print("Indice inválido")
 
                 case "8":
-                    if self.__quadro is None:
-                        print("Necessário criar um quadro antes de realizar esta ação!")
+                    if not self.__verificar_quadro():
+                        continue
                     else:
                         for i, tarefa in enumerate(self.__quadro.listar_tarefas(), start=1):
                             print(f"{i}. {tarefa}\n")
 
                 case "9":
-                    if self.__quadro is None:
-                        print("Necessário criar um quadro antes de realizar esta ação!")
+                    if not self.__verificar_quadro():
+                        continue
                     else:
                         for i, tarefa in enumerate(self.__quadro.listar_tarefas(), start=1):
                             print (f"{i}. {tarefa.exibir_detalhes()}\n")
-
+                        
                 case "10":
-                    print("Encerrado sistema...")
+                    if not self.__verificar_quadro():
+                        continue
+                    else:
+                        tarefas = self.__quadro.listar_tarefas()
+                        for i, tarefa in enumerate(self.__quadro.listar_tarefas(), start=1):
+                            if isinstance(tarefa, TarefaAcademica):
+                                print(f"{i}. {tarefa}")
+                        indice = int(input("Insira a seguir o indice da tarefa que deseja verificar o prazo: "))
+                        if 1<= indice <= len(tarefas):
+                                print(tarefas[indice - 1].verificar_prazo())
+                        else:
+                            print("Indice invalido")
+
+                case "11":
+                    print("Encerrando sistema...")
                     break
 
                 case _:
